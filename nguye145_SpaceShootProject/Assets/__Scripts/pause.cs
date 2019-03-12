f1b622ebb9a0105fb8afcebb46aadccc784f29d1
Anthony Nguyen <46362634+AnthonyN3@users.noreply.github.com>
2019-03-11 22:51:06 -0400

    Adding menu,options,configurations...
    -Beginning to add a pause key bind

---
+++ nguye145_SpaceShootProject/Assets/__Scripts/pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+	public bool paused;
+
+    public GameObject pauseCanvas;
+
+    void Start () {
+	paused = false;
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Escape))
+				paused = !paused;			
+
+		// or just do (pause)
+		if (paused == true){
+			Time.timeScale = 0;
+			pauseCanvas.SetActive (true);
+
+		}
+		// or just do (!pause)
+		else if (paused == false){
+			Time.timeScale  = 1;
+			pauseCanvas.SetActive(false); 
+		}
+	}
+}
